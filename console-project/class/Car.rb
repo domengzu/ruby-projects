635916b7c61f09e4b5b7e9a47f468c5a37ee371c
dom.dom <dyasuham@gmail.com>
2026-01-28 14:44:41 +0800

    Refactor something idk

--- console-project/class/Car.rb
+++ console-project/class/Car.rb
@@ -1,5 +1,19 @@
 class Car
-  def start
-    puts "Engine Started"
+  @@cars_array = []
+
+  attr_accessor :model, :year
+
+  def initialize(model, year)
+    @model = model
+    @year = year
+    @@cars_array << self
   end
+  def self.all
+    @@cars_array
+  end
+
+  # def info
+  #   puts "Your car model is: #{@model}"
+  #   puts "Car year make: #{@year}"
+  # end
 end
--- console-project/main.rb
+++ console-project/main.rb
@@ -1,4 +1,11 @@
 require_relative 'class/Car' # => require_relative used for local file's path
 # require './class/Car' # => require is used if the file is in another folder 
-supra = Car.new
-supra.start
+print "Enter model of the car: "
+model = gets.chomp
+
+print "What year is it made? "
+year = gets.chomp
+
+Car.new(model, year)
+Car.all.each {|car| puts "[#{car.model}, #{car.year}]"}
+# car.info
