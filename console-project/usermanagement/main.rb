7feaf9cc65bbdd3f388bf07c44ef3d4f68599b9f
dom.dom <dyasuham@gmail.com>
2026-02-04 15:20:33 +0800

    chore: added another user management project using OOP

---
+++ console-project/usermanagement/main.rb
@@ -0,0 +1,58 @@
+require_relative 'user'
+
+user = UserManagement.new
+
+puts "\n# ------ USER MANAGEMENT ----- #"
+
+loop do 
+  puts "1. View users"
+  puts "2. Search user"
+  puts "3. Add user"
+  puts "4. Edit user"
+  puts "5. Delete user"
+  puts "6. Exit"
+  print "Enter your selected number: "
+  choice = gets.chomp.to_i
+    
+  case choice
+  when 1
+    user.view_users
+  when 2
+    puts "\n# ====== SEARCH USER ====== #"
+    print "Enter name: "
+    name = gets.chomp
+    name.capitalize!
+    user.search_user(name) 
+  when 3
+    puts "\n# ====== ADD USER ====== #"
+    print "Enter name of the user: "
+    username = gets.chomp.capitalize
+    
+    user.add_user(username)
+    # display user
+    user.view_users
+
+    puts "User added succesfully!"
+  when 4
+    puts "\n# ====== UPDATE USER ====== #"
+    user.view_users
+    print "Enter ID: "
+    id = gets.chomp.to_i
+    print "Enter new name: "
+    new_name = gets.chomp
+    user.update_user(id, new_name)
+  when 5
+    puts "\n# ====== DELETE USER ====== #"
+    user.view_users
+    print "Enter ID: "
+    id = gets.chomp.to_i
+    user.delete_user(id)
+  when 6
+    puts "Exit"
+    break
+  else
+    message = "\nTaronga imong input choi ayawg lapas sa choices, barumbado!"
+    puts message.upcase!
+    puts "\n"
+  end
+end
---
+++ console-project/usermanagement/user.rb
@@ -0,0 +1,61 @@
+class UserManagement
+  
+  def initialize
+    @users = []
+    # @name = name
+  end
+
+  def add_user(name)
+    @users << name
+  end
+
+  def view_users
+    if @users.empty?
+      puts "\n[No users found.]"
+      puts "\n"
+    else
+      puts "\n# ====== LIST OF USERS ====== #"
+      @users.each_with_index {|user, index| puts "#{index + 1}. #{user}"}
+      puts "\n"
+    end
+  end
+
+  def update_user(id, new_name)
+    index = id - 1
+    
+    if index >= 0 && index < @users.length
+      @users[index] = new_name.capitalize!
+      puts "\nUser name updated successfully!"
+      view_users
+    else
+      puts "\nInvalid ID"
+      puts ""
+    end
+  end
+
+  def delete_user(id)
+    index = id - 1
+
+    if index >= 0 && index < @users.length
+      @users.delete_at(index)
+      puts "\nUser deleted successfully"
+      view_users
+    else
+      puts "\nAction unsuccessful. Invalid ID."
+      puts ""
+    end
+  end
+
+  def search_user(query)
+    result = @users.select{|user| user == query}
+
+    if result.empty?
+      puts "\nNo users found."
+      puts ""
+    else
+      puts result.size == 1 ? "\n#{result.size} User found!" : "\n#{result.size} Users found!"
+      result.each_with_index {|user, index| puts "#{index + 1}. #{user}"}
+      puts ""
+    end
+  end
+end
